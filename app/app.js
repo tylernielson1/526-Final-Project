06c451f09f52f4f13b5d927688795671699b9d91
Tyler Nielson <tyler.snielson@gmail.com>
2018-03-24 21:56:05 -0500

    starter project going. Most dependencies installed.

--- app/app.js
+++ app/app.js
@@ -2,10 +2,13 @@ const express = require("express")
 const path = require("path")
 const server = express()
 
+server.set('view engine', 'ejs');
 server.use('/static', express.static(path.join(__dirname, 'static')));
 
 server.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, 'public/index.html'))
+	res.render(path.join(__dirname, 'public/index.ejs'),{name: "Tyler"})
 })
 
-server.listen(3000)
+server.listen(3000, function() {
+	console.log("Listening on port 3000")
+})
