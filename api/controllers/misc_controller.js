be95e1a40358e4d6b093e85b497c8f87dc3d1eaf
Tyler Nielson <nielson@users.campus>
2018-04-04 11:45:45 -0500

    added team name endpoint controller

--- api/controllers/misc_controller.js
+++ api/controllers/misc_controller.js
@@ -3,6 +3,9 @@ const pool = require('../../db/connection')
 const GET_COUNTRY_NAMES_QUERY = "SELECT countries.name\n"
                                 + "FROM countries;"
 
+const GET_TEAM_NAMES_QUERY = "SELECT teams.name\n"
+                            + "FROM teams;"
+
 function getCountries(req, res) {
     pool.query(GET_COUNTRY_NAMES_QUERY, function(error, results, fields) {
         if (error) {
@@ -13,6 +16,17 @@ function getCountries(req, res) {
     })
 }
 
+function getTeamNames(req, res) {
+    pool.query(GET_TEAM_NAMES_QUERY, function(error, results, fields) {
+        if (error) {
+            console.error("Error submitting database query.")
+            res.statusCode = 404
+        }
+        res.json(results)
+    })
+}
+
 module.exports = {
-    getCountries
+    getCountries,
+    getTeamNames
 }
