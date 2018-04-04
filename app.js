e07ff7a3405d6942fbb1815f0dcb757fc5a57914
Tyler Nielson <tylernielson1@users.noreply.github.com>
2018-04-03 23:19:13 -0500

    Front end design1 (#1)
    * got rid of bootstrap (not worried about looks yet). Finished navbar and made it responsive.
    
    * rough homepage done
    
    * started a bit of api work
    
    * finishing up player and team search pages
    
    * did more work on searchplayer.ejs. Advanced search is almost done
    
    * finished player search page
    
    * team search page done
    
    * Added in team endpoints
    
    * don't remember what changes I made lol
    
    * finished player search results. Currently has dummy data for players
    
    * finished team results page and populated with dummy data.
    
    * added league and stadium pages.
    
    * finalized front end rough draft for open house. Still need to clean it up after open house for the final due date.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const SwaggerUi = require('swagger-tools/middleware/swagger-ui')
 const express = require('express')
 const config = require('./config/config')
 const path = require('path')
+const bodyParser = require('body-parser')
 const server = express()
 
 var swagger_config = {
@@ -17,15 +18,70 @@ SwaggerExpress.create(swagger_config, function(err, swaggerExpress) {
     //installs middleware
     swaggerExpress.register(server)
     server.use(SwaggerUi(swaggerExpress.runner.swagger))
+    server.use(bodyParser.urlencoded({ extended: true }))
 
+    //sets the view engine to use ejs and sets the static directory for css, js, and images.
     server.set('view engine', 'ejs')
     server.use('/static', express.static(path.join(__dirname, 'static')))
 
-
+    //establishes all of the get endpoints.
     server.get('/', function(req, res) {
-        res.render(path.join(__dirname, 'public/index.ejs'),{name: "Tyler"})
+        res.render(path.join(__dirname, 'public/index.ejs'), {})
     })
 
+    server.get('/playerinfo', function(req, res) {
+        res.render(path.join(__dirname, 'public/player.ejs'), {})
+    })
+
+    server.get('/teaminfo', function(req, res) {
+        res.render(path.join(__dirname, 'public/team.ejs'), {})
+    })
+
+    server.get('/searchplayers', function(req, res) {
+        res.render(path.join(__dirname, 'public/searchplayer.ejs'), {})
+    })
+
+    server.get('/searchteams', function(req, res) {
+        res.render(path.join(__dirname, 'public/searchteam.ejs'), {})
+    })
+
+    server.all('/playerresults', function(req, res) {
+        if(req.method == "POST") {
+            console.log(req.body.playerName)
+        }
+        else if (req.method != "GET") {
+            res.end();
+        }
+        res.render(path.join(__dirname, 'public/playerresults.ejs'), {})
+    })
+
+    server.get('/loader', function(req, res) {
+        res.render(path.join(__dirname, 'public/loader.ejs'), {})
+    })
+
+    server.get('/teamresults', function(req, res) {
+        res.render(path.join(__dirname, 'public/teamresults.ejs'), {})
+    })
+
+    server.get('/login', function(req, res) {
+        res.render(path.join(__dirname, 'public/login.ejs'), {})
+    })
+
+    server.get('/signup', function(req, res) {
+        res.render(path.join(__dirname, 'public/signup.ejs'), {})
+    })
+
+    //establishes all of the post endpoints.
+
+    /*server.post('/playerresults', function(req, res) {
+        res.render(path.join(__dirname, 'public/playerresults.ejs'), {user: req.body.playerName})
+    })*/
+
+    server.post('/teamresults', function(req, res) {
+        res.render(path.join(__dirname, 'public/teamresults.ejs'), {})
+    })
+
+    //configures the port number and starts the server.
     var port = config.app.port
     server.listen(port, function() {
         console.log(`Listening on port ${port}`)
